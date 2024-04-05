59cc99f85f9ab1fba4b55ff4ff1bf0775dea0363
Gonzalo Garramuño <ggarra13@gmail.com>
2024-04-05 08:18:16 -0300

    Updated source code.

--- pxr/usd/bin/usdotio/lib/python/schema/functions.py
+++ pxr/usd/bin/usdotio/lib/python/schema/functions.py
@@ -1,25 +1,3 @@
-# Copyright 2024 Gonzalo Garramuño for Signly, Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "Apache License")
-# with the following modification; you may not use this file except in
-# compliance with the Apache License and the following modification to it:
-# Section 6. Trademarks. is deleted and replaced with:
-#
-# 6. Trademarks. This License does not grant permission to use the trade
-#    names, trademarks, service marks, or product names of the Licensor
-#    and its affiliates, except as required to comply with Section 4(c) of
-#    the License and to reproduce the content of the NOTICE file.
-#
-# You may obtain a copy of the Apache License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the Apache License with the above modification is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied. See the Apache License for the specific
-# language governing permissions and limitations under the Apache License.
-#
 
 
 def get_optional_timecode(usd_prim, usd_attr):
@@ -68,3 +46,10 @@ def get_string(usd_prim, usd_attr):
         print(f'{usd_prim} Invalid string attribute "{usd_attr}"')
         return None
     return attr.Get()
+
+#
+# Temporary comparison function as we don't load the omni libraries.
+#
+def is_a(usd_prim, usd_type):
+    usd_type_name = usd_prim.GetTypeName()
+    return usd_type_name == usd_type
